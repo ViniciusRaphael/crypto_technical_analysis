07a0fe012248e6328dfcc64130f99835527867b2
Vinicius Raphael <vinicius.raphael92@gmail.com>
2024-02-15 16:40:54 -0300

    Improving code and editing final result for return a dataframe with less columns

--- scripts/indicators.py
+++ scripts/indicators.py
@@ -1,12 +1,13 @@
 # Python standard libraries
+from datetime import datetime, timedelta
+import time
 from configparser import ConfigParser
 
 # Third-party libraries
 import pandas as pd
 import pandas_ta as ta
-from sqlalchemy import text
 import plotly.graph_objects as go
-import time
+from sqlalchemy import text
 
 # My own libraries or custom modules
 import db_util as du
@@ -38,6 +39,7 @@ def classify_adx_value(value):
             return name
     return None
 
+
 def count_positive_reset(df_column):
     """
     Counts consecutive positive values in a DataFrame column and resets count on encountering negative values.
@@ -60,39 +62,44 @@ def count_positive_reset(df_column):
 
     return counts
 
+
 def add_indicators(dataframe):
+    """
+    Add technical indicators to the DataFrame.
+
+    Parameters:
+    - dataframe (pd.DataFrame): DataFrame containing financial data.
+
+    Returns:
+    - pd.DataFrame: Updated DataFrame with added technical indicators.
+    """    
     # Sort DataFrame by 'symbol' and 'date', convert date column to datetime
     dataframe[['open', 'close', 'high', 'low']] = dataframe[['open', 'close', 'high', 'low']].astype(float)
     dataframe = dataframe.sort_values(by=['symbol', 'date']).reset_index(drop=True)
     dataframe['date'] = pd.to_datetime(dataframe['date'])
 
     # Calculate and add SMAs
-    for window in [20, 50, 100, 200]:
-        dataframe[f'ma_{window}'] = dataframe.groupby('symbol')['close'].transform(lambda x: ta.sma(x, window))
+    # for window in [20, 50, 100, 200]:
+    #     dataframe[f'ma_{window}'] = dataframe.groupby('symbol')['close'].transform(lambda x: ta.sma(x, window))
 
     # Calculate and add the minimum and maximum values for a 50-day rolling window
     dataframe['min_50'] = dataframe.groupby('symbol')['close'].transform(lambda x: x.shift(1).rolling(window=50).min())
     dataframe['max_50'] = dataframe.groupby('symbol')['close'].transform(lambda x: x.shift(1).rolling(window=50).max())
 
     # Calculate ADX
-    adx_values = dataframe.groupby('symbol').apply(lambda x: ta.adx(x['high'], x['low'], x['close'], length=14)).reset_index(drop=True)
-    dataframe[['vl_adx', 'vl_dmp', 'vl_dmn']] = adx_values
+    dataframe[['vl_adx', 'vl_dmp', 'vl_dmn']] = dataframe.groupby('symbol').apply(lambda x: ta.adx(x['high'], x['low'], x['close'], length=14)).reset_index(drop=True)
     dataframe['nm_adx_trend'] = dataframe['vl_adx'].transform(classify_adx_value)
 
     # Calculate RSI
     dataframe['rsi'] = dataframe.groupby('symbol')['close'].transform(lambda x: ta.rsi(x))
 
     # Calculate Ichimoku Cloud indicators
-    ichimoku_values = dataframe.groupby('symbol').apply(lambda x: ta.ichimoku(x['high'], x['low'], x['close'])[0]).reset_index(drop=True)
-    dataframe[['vl_leading_span_a', 'vl_leading_span_b', 'vl_conversion_line', 'vl_base_line', 'vl_lagging_span']] = ichimoku_values
-    dataframe[['vl_leading_span_a', 'vl_leading_span_b', 'vl_conversion_line', 'vl_base_line', 'vl_lagging_span']] = dataframe[['vl_leading_span_a', 'vl_leading_span_b', 'vl_conversion_line', 'vl_base_line', 'vl_lagging_span']].astype(float)
+    dataframe[['vl_leading_span_a', 'vl_leading_span_b', 'vl_conversion_line', 'vl_base_line', 'vl_lagging_span']] = dataframe.groupby('symbol').apply(lambda x: ta.ichimoku(x['high'], x['low'], x['close'])[0]).reset_index(drop=True)
     dataframe['vl_price_over_conv_line'] = dataframe['close'] - dataframe['vl_conversion_line']
     dataframe['qt_days_ichimoku_positive'] = count_positive_reset(dataframe['vl_price_over_conv_line'])
 
-    # Drop unnecessary columns
-    dataframe = dataframe.drop(columns=['vl_dmp', 'vl_dmn'])
-
     return dataframe
+
 
 def filter_indicators_today(dataframe, vl_adx_min = 25, date = '2024-02-12' ,vl_macd_hist_min = 0, vl_macd_delta_min = 0.01, qt_days_supertrend_positive = 1):
     """
@@ -107,7 +114,7 @@ def filter_indicators_today(dataframe, vl_adx_min = 25, date = '2024-02-12' ,vl_
 
     df_indicators = dataframe.loc[
         (dataframe['vl_adx'] >= vl_adx_min) &
-        (dataframe['date'] == date) &
+        (dataframe['date'] == str(date)) &
 
         # Ichimoku with price above conversion line and base line
         (dataframe['close'] > dataframe['vl_conversion_line']) &
@@ -118,6 +125,21 @@ def filter_indicators_today(dataframe, vl_adx_min = 25, date = '2024-02-12' ,vl_
         # (dataframe['vl_macd_delta'] >= vl_macd_delta_min) &
         # (dataframe['qt_days_supertrend_positive'] >= qt_days_supertrend_positive)
     ]
+
+        # Drop unnecessary columns
+    df_indicators = df_indicators.drop(
+        columns = [
+            'high',
+            'low',
+            'volume',
+            'dividends',
+            'vl_dmp', 
+            'vl_dmn', 
+            'vl_leading_span_a', 
+            'vl_leading_span_b', 
+            'vl_lagging_span'
+            ]
+        )
 
     return df_indicators
 
@@ -145,5 +167,6 @@ with engine.connect() as conn:
     end = time.time()
     print(f'Code finished in: {end - start} sec')
 
-    result1 = filter_indicators_today(df1)
+    yesterday_date = datetime.today().date() - timedelta(days=1)
+    result = filter_indicators_today(df1, date = yesterday_date)
     
