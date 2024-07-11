ce467cb514c83b6d35ae69e1edd4991c71dc75b8
Allysson Macedo <allyssonvsmacedo@gmail.com>
2024-07-10 23:15:16 -0300

    comment outputs in the crypto indicators

--- scripts/utils/2.crypto_data_with_indicators.py
+++ scripts/utils/2.crypto_data_with_indicators.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import pyarrow as pa
 import pyarrow.parquet as pq
 import pandas as pd
-pd.set_option("display.max_columns", None) 
-pd.set_option("display.max_rows", None) 
+# pd.set_option("display.max_columns", None) 
+# pd.set_option("display.max_rows", None) 
 
 from indicators_util import add_indicators
 
@@ -51,7 +51,7 @@ def main():
         output_path = Path(output_folder) / output_file
 
         # Save the DataFrame with indicators as a Parquet file
-        print(indicators_dataframe)
+        # print(indicators_dataframe)
         save_dataframe_to_parquet(indicators_dataframe, output_path)        
         
         print(f"Parquet file with indicators saved to {output_path}")
