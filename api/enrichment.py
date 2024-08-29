ce671b9f8ced16b3aafa5abd9783e5b0eadcc7b2
Allysson Macedo <94933158+allyssonmacedo@users.noreply.github.com>
2024-08-28 23:22:37 -0300

    chaging nothing, just to delete the branch (#46)

--- api/enrichment.py
+++ api/enrichment.py
@@ -3,7 +3,6 @@ import parameters
 # instancia as classes
 cls_File = parameters.cls_FileHandling
 cls_Models = parameters.cls_Models
-cls_DataPrep = parameters.cls_DataPrep
 
 
 if parameters.execute_data_ingestion:
